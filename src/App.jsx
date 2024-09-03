61a75f0d4b36aa7eb9666467badc793d9fc2cea2
Brian Cloutier <bmcloutier@gmail.com>
2024-09-03 14:24:16 -0500

    Added signup, login, logout components, and installed react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 import { Header } from "./Header";
+import { SignupPage } from "./SignupPage";
+import { LoginPage } from "./LoginPage";
 import { ResumesPage } from "./ResumesPage";
 import { Footer } from "./Footer";
 
+const router = createBrowserRouter([
+  {
+    element: (
+      <div>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <ResumesPage />,
+      },
+      {
+        path: "/signup",
+        element: <SignupPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      <Header />
-      <ResumesPage />
-      <Footer />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
