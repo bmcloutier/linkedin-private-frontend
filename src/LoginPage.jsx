c2f1019494482ce56884841011b87a6ee59ef181
merrelltd72 <tyron.merrell@gmail.com>
2024-09-04 13:46:39 -0500

    Added TailwindCSS stylings.

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -17,7 +17,8 @@ export function LoginPage() {
       .post("http://localhost:3000/sessions.json", params)
       .then((response) => {
         console.log(response.data);
-        axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+        axios.defaults.headers.common["Authorization"] =
+          "Bearer " + response.data.jwt;
         localStorage.setItem("jwt", response.data.jwt);
         event.target.reset();
         window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
@@ -29,21 +30,39 @@ export function LoginPage() {
   };
 
   return (
-    <div id="login">
-      <h1>Login</h1>
+    <div id="login" className="container w-full max-w-sm">
+      <h1 className="text-sm lg:flex-grow">Login</h1>
       <ul>
         {errors.map((error) => (
           <li key={error}>{error}</li>
         ))}
       </ul>
-      <form onSubmit={handleSubmit}>
-        <div>
-          Email: <input name="email" type="email" />
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <div className="block text-gray-600 text-lg font-bold mb-2">
+          Email:{" "}
+          <input
+            name="email"
+            type="email"
+            className="shadow appearance-none border rounded w-full py-1 px-3 text-gray-600 leading-tight focus:outline-none focus:shadow-outline"
+          />
         </div>
-        <div>
-          Password: <input name="password" type="password" />
+        <div className="block text-gray-600 text-lg font-bold mb-2">
+          Password:{" "}
+          <input
+            name="password"
+            type="password"
+            className="shadow appearance-none border rounded w-full py-1 px-3 text-gray-600 leading-tight focus:outline-none focus:shadow-outline"
+          />
         </div>
-        <button type="submit">Login</button>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-green-700 text-white font-bold rounded py-2 px-4"
+        >
+          Login
+        </button>
       </form>
     </div>
   );
